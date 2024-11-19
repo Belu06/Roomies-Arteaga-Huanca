83721316ca4f7cda196db8971b9d72bbba380726
abrilArteaga <arteagaabruu@gmail.com>
2024-11-19 20:15:49 -0300

    le agregue el insert en viviendas

--- db/inserta_viviendas.mjs
+++ db/inserta_viviendas.mjs
@@ -1,1 +1,13 @@
-import db from "@/db"
+import { PrismaClient } from "@prisma/client"
+(async () => {
+    try {
+const db = new PrismaClient()
+await db.$connect()
+await db.viviendas.create({data:{
+    direccion:"palermo",locatario:"Roberto",precioDeAlquiler:"20.000"
+}})
+await db.$disconnect()
+    } catch (error) {
+        console.log("no se pudo crear las viviendas")
+    }
+})()
